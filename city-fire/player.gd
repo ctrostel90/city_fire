0923c5eca19fd8ee57833109a3c5034148ed37d4
dips-fw <trostel.connor@gmail.com>
2026-06-29 20:35:30 -0400

    initial commit

---
+++ city-fire/player.gd
@@ -0,0 +1,57 @@
+class_name player
+extends CharacterBody2D
+
+@onready var left_wheel: Sprite2D = $front_wheel/left
+@onready var right_wheel: Sprite2D = $front_wheel/right
+@export var wheel_base := 64
+@export var engine_power := 900
+@export var steering_limits := Vector2(-45, 45)
+var steering_angle := 15.0
+var acceleration := Vector2.ZERO
+var friction := -55
+var drag = -0.06
+var steer_direction
+var braking := -450.0
+var max_speed_reverse := 250
+
+
+func _physics_process(delta: float) -> void:
+	acceleration = Vector2.ZERO
+	get_input()
+	apply_friction(delta)
+	calculate_steering(delta)
+	velocity += acceleration * delta
+	move_and_slide()
+
+
+func apply_friction(delta):
+	if acceleration == Vector2.ZERO and velocity.length() < 50:
+		velocity = Vector2.ZERO
+	var friction_force = velocity * friction * delta
+	var drag_force = velocity * velocity.length() * drag * delta
+	acceleration += drag_force + friction_force
+
+
+func get_input() -> void:
+	var dir := Input.get_axis("steer_left", "steer_right") # / 2.0 + 0.5
+	steer_direction = dir * deg_to_rad(steering_angle)
+	left_wheel.rotation = steer_direction
+	right_wheel.rotation = steer_direction
+	if Input.is_action_pressed("accelerate"):
+		acceleration = transform.x * engine_power
+	if Input.is_action_pressed("braking"):
+		acceleration = transform.x * braking
+
+
+func calculate_steering(delta):
+	var rear_wheel := position - transform.x * wheel_base / 2.0
+	var front_wheel := position + transform.x * wheel_base / 2.0
+	rear_wheel += velocity * delta
+	front_wheel += velocity.rotated(steer_direction) * delta
+	var new_heading := (front_wheel - rear_wheel).normalized()
+	var direction := new_heading.dot(velocity.normalized())
+	if direction > 0:
+		velocity = new_heading * velocity.length()
+	else:
+		velocity = -new_heading * min(velocity.length(), max_speed_reverse)
+	rotation = new_heading.angle()
---
+++ notes.md
@@ -0,0 +1,13 @@
+# City Fire idea
+
+Control a firetruck in a major city. multiple players is the goal. 
+- One player steers the front of the truck
+- One player steers the back
+- avoid hitting buildings or other things
+- different responsibilities is the key
+- think overcooked but controlling a firetruck trying to put out a fire
+- someone has to aim the hose
+- someone needs to hook up the hose
+- extend/control the ladder
+- brin gout the cat saving trampoline
+- score points on a level etc
